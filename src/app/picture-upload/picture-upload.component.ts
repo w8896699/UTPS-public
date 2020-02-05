edcd43f942083b27ace431c31ad597fc0497cc31
Billy Li <billy.li.901@gmail.com>
2020-02-05 12:40:10 +0800

    add a drop-drag component

---
+++ src/app/picture-upload/picture-upload.component.ts
@@ -0,0 +1,24 @@
+//
+// reference http://www.advancesharp.com/blog/1218/angular-4-upload-files-with-data-and-web-api-by-drag-drop
+//
+import { Component, Input, Output, EventEmitter, HostListener, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-picture-upload',
+  templateUrl: './picture-upload.component.html',
+  styleUrls: ['./picture-upload.component.scss']
+})
+export class PictureUploadComponent {
+  public files: any = [];
+  constructor() { }
+
+  uploadFile(event) {
+    for (let index = 0; index < event.length; index++) {
+      const element = event[index];
+      this.files.push(element.name);
+    }
+  }
+  deleteAttachment(index) {
+    this.files.splice(index, 1);
+  }
+}
