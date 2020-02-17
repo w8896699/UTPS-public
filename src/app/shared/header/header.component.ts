efe647873af93a7988d0364bc456096cb5236123
Billy Li <billy.li.901@gmail.com>
2020-02-17 21:47:10 +0800

    refresh page after add an item

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit,ChangeDetectorRef} from '@angular/core';
+import {Component, OnDestroy, OnInit, ChangeDetectorRef} from '@angular/core';
 import {LoginComponent} from '../../login/login.component';
 import {NgbModal, NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
 // import {Subscription} from 'rxjs';
@@ -18,31 +18,31 @@ export class HeaderComponent implements OnInit {
   constructor(
     private ngbService: NgbModal,
     private loginService: LoginService,
-    private ChangeDetectorRef: ChangeDetectorRef,
+    private changeDetectorRef: ChangeDetectorRef,
     private router: Router
     // private userService: UserService
   ) {
   }
 
   ngOnInit() {
-    this.loginEvent = localStorage.getItem('currentUser') !== null; //check if user logged in
+    this.loginEvent = localStorage.getItem('currentUser') !== null; // check if user logged in
     // console.log('wtf',this.loginEvent);
   }
 
   logout() {
     this.loginService.logout();
     this.router.navigate(['poster']);
-    this.loginEvent = localStorage.getItem('currentUser') !== null; //ture for loged in false for not
-    this.ChangeDetectorRef.detectChanges();
+    this.loginEvent = localStorage.getItem('currentUser') !== null; // ture for loged in false for not
+    this.changeDetectorRef.detectChanges();
   }
   login() {
     this.ngbModal = this.ngbService.open(LoginComponent, {size: 'lg'});
     this.ngbModal.result.then((item) => {
-      console.log("User close this windows", item);//which I haven;t add close button yet
-    },(item)=>{ //background dissmiss item is 0, 
-      this.loginEvent = localStorage.getItem('currentUser') !== null; //ture for loged in false for not
-    })
-    this.ChangeDetectorRef.detectChanges();
+      console.log('User close this windows', item); // which I haven;t add close button yet
+    }, (item) => { // background dissmiss item is 0
+      this.loginEvent = localStorage.getItem('currentUser') !== null; // ture for loged in false for not
+    });
+    this.changeDetectorRef.detectChanges();
   }
 
 
